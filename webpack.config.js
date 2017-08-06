afbed2c78230b5eee9c52e7fd0a92b0ccff22baf
Seth Nickell <snickell@gmail.com>
2017-08-06 12:37:51 -0700

    Add babel to webpack for JSX

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,16 @@ module.exports = {
     entry: {
         background: "./src/background.js",
         tab: "./src/tab.js",
-        content: "./src/content.js"
+        content: "./src/content.js",
     },
     output: {
         path: path.resolve(__dirname, "addon/build"),
         filename: "[name]/index.js"
-    }
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      ]
+    }    
 };
