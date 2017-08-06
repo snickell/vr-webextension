67910edbd4344c515664b209986ed3e124452126
Seth Nickell <snickell@gmail.com>
2017-08-06 04:27:06 -0700

    Multiple tab support

--- src/tab.js
+++ src/tab.js
@@ -1,10 +1,10 @@
 console.log("Creating tab");
 
 const senderIDToCanvas = new Map();
+const canvasToScreen = new Map();
 
 document.addEventListener("DOMContentLoaded", function(event) {
     const canvasHeader = document.getElementById('preview');
-        console.log("ping");
     const canvasContainer = document.getElementById('previewCanvases');
     canvasHeader.addEventListener('mouseenter', function () {
         canvasContainer.style.display = 'block';
@@ -13,7 +13,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
         canvasContainer.style.display = 'none';
     });
 
-    const threeScreen = document.getElementById('screen').object3D.children[0];
+    //const threeScreen = document.getElementById('screen').object3D.children[0];
+    const screens = document.getElementById('screens');
+
+
+
+    // <a-image id="screen" position="0 1 -2" rotation="0 0 0" width="2" height="3" src="./sample.jpg" ></a-image>
+
+    function addNewScreen(srcCanvas) {
+        console.log("addNewScreen()");
+        const numScreens = senderIDToCanvas.size;
+    
+        var newScreen = document.createElement('a-image');
+        newScreen.className = "screen";
+        newScreen.setAttribute("position", "0 1 -2");
+        newScreen.setAttribute("width", "2");
+        newScreen.setAttribute("height", "3");
+        newScreen.setAttribute("src", "./sample.jpg");
+
+        screens.appendChild(newScreen);
+                
+        canvasToScreen.set(srcCanvas, newScreen);
+
+        positionScreens();    
+    }
+    
+    function positionScreens() {
+        const screens = Array.from(canvasToScreen.values());
+        
+        // for now, position them equally spaced on a circle
+        const theta = ((Math.PI*2) / screens.length);        
+        screens.forEach((scr, idx) => {
+            const radius = 2;
+            const angle = theta * idx;
+            const angleDeg = 360.0 - (theta * idx * (180.0 / Math.PI));
+            
+            const x = radius * Math.sin(angle);
+            const y = 1;            
+            const z = -radius * Math.cos(angle);
+            
+            const position = `${x} ${y} ${z}`;
+            scr.setAttribute("position", position);
+            
+            const rotation = `0 ${angleDeg} 0`;
+            scr.setAttribute("rotation", rotation);
+        });
+    }
+
 
     function handleMessage(request, sender, sendResponse) {
         const id = sender.tab.id;
@@ -23,8 +69,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
             newCanvas.classList.add('tab');
             canvasContainer.appendChild(newCanvas);
             senderIDToCanvas.set(id, newCanvas);
+            addNewScreen(newCanvas);
         }
-
+        
         const canvas = senderIDToCanvas.get(id);
         const ctx = canvas.getContext("2d");
 
@@ -42,11 +89,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         //texture.wrapS = THREE.RepeatWrapping;
         //texture.repeat.x = - 1;        
         
+        const threeScreen = canvasToScreen.get(canvas).object3D.children[0];
         threeScreen.material.map = texture;
 
         
         sendResponse({ response: "from tab", sender: sender});
     }
+    
+    console.log("lunk");
+    
 
     browser.runtime.onMessage.addListener(handleMessage);
 });
