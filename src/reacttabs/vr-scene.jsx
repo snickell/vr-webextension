beb13c6abd02477762dece10905445101a486eaa
snickell <snickell@gmail.com>
2017-08-07 15:40:39 -0700

    Use correct class for react TabScreens

--- src/reacttabs/vr-scene.jsx
+++ src/reacttabs/vr-scene.jsx
@@ -9,12 +9,12 @@ export default class VRScene extends React.Component {
       <Scene>
         <div style={{position: "absolute", right: "0px", top: "0px", zIndex: 10, color: "white", padding: "3px"}}>GIT: {__GIT_REVISION__}</div>
         <a-camera>
-          <a-cursor raycaster="objects: .screen; showLine: true"></a-cursor>
+          <a-cursor raycaster="objects: .tab-screen; showLine: true"></a-cursor>
         </a-camera>
 
         <Entity>
-          <Entity laser-controls={{hand: "left"}} raycaster={{objects: ".screen", showLine: true}}/>
-          <Entity laser-controls={{hand: "right"}} raycaster={{objects: ".screen", showLine: true}}/>
+          <Entity laser-controls={{hand: "left"}} raycaster={{objects: ".tab-screen", showLine: true}}/>
+          <Entity laser-controls={{hand: "right"}} raycaster={{objects: ".tab-screen", showLine: true}}/>
         </Entity>
     
         <TabScreens/>
